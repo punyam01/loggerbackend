1e4aac92b423dd7c6e558b54a269ef92934762b8
Punyam kumar <punyamkumarsingh@gmail.com>
2025-08-20 11:30:20 +0530

    changing cookie option in prod

--- utils/cookie.utils.js
+++ utils/cookie.utils.js
@@ -1,6 +1,7 @@
 const cookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === 'production' // Only use secure in production
+  secure: process.env.NODE_ENV === 'production', // must be true on HTTPS
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax' // cross-site cookies in prod
 }
 
 export { cookieOptions }
